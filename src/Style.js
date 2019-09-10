ea4ac36541474a5047a66f23b741d2e98467ace7
Mark Dane <mark.dane@gmail.com>
2019-09-10 19:46:33 -0400

    removed unused function from style

--- src/Style.js
+++ src/Style.js
@@ -1,29 +1,3 @@
-
-
-
-export function getStyleLink() {
-    const link = document.createElement('link');
-    link.setAttribute('href', 'webapp-menu.css');
-    link.setAttribute('rel', 'stylesheet');
-    return link;
-}
-
-export function reusableStyleSheetsFunction(styleStringArray) {
-    var stylesheets;
-    if( ! Array.isArray(styleStringArray))
-        styleStringArray = [styleStringArray];
-    return function() {
-        if( ! stylesheets) {
-            stylesheets = styleStringArray.map(styleString=>{
-                const sheet = new CSSStyleSheet();
-                sheet.replaceSync(styleString);
-                return sheet;
-            })
-        }
-        return stylesheets;
-    }
-}
-
 export class ReusableStyleSheet {
     constructor(styleText) {
         this.text = styleText;
