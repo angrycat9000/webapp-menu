3127ced3d93a017de32ac8a0fe389ebae76408e8
Mark Dane <mark.dane@gmail.com>
2019-09-12 08:43:09 -0400

    Cleaned up some unused files

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import Menu from './Menu';
+import {Menu, Direction} from './Menu';
 import Item from './Item';
 import Position from './Position';
 import Toolbar from './Toolbar';
@@ -6,11 +6,10 @@ import Popup from './Popup';
 import SubMenuItem from './SubMenuItem';
 import TreeList from './TreeList';
 
-const menuExport = {Item, SubMenuItem, Popup, Toolbar, Position, Menu, TreeList};
+const menuExport = {Item, SubMenuItem, Popup, Toolbar, Position, Menu, Direction, TreeList};
 
 for(let e in menuExport) {
     const c = menuExport[e];
-    //console.debug(c, c.tagName, Item.tagName);
     if(c.tagName) {
         window.customElements.define(c.tagName, c);
         console.debug(`Registering <${c.tagName}>` );
--- src/loader.js
+++
@@ -1,6 +0,0 @@
-import Menu from './api.js';
-
-for(let element of [Menu.Item, Menu.SubMenuItem, Menu.Toolbar, Menu.Popup]) {
-    window.customElements.define(element.tagName, element);
-    console.debug(`Registering <${element.tagName}>` );
-}
