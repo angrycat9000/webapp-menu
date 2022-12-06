ca30d23b26336c6fc122bfe5b5f66f70fdb387d5
Mark Dane <mark.dane@gmail.com>
2022-12-06 11:09:25 -0500

    More FocusList tests

--- tests/FocusList.test.js
+++ tests/FocusList.test.js
@@ -2,19 +2,22 @@ import { expect, fixture, html } from "@open-wc/testing";
 import '../dist/webapp-menu';
 
 describe("FocusList", () => {
-  let list;
+  let menu, list, itemOne, itemTwo, itemThree;
   beforeEach(async () => {
-    const parent = await fixture(
+    menu = await fixture(
       html`<wam-menu>
-        <wam-item id="one"></wam-item>npmm 
+        <wam-item id="one"></wam-item>
         <wam-item id="two"></wam-item>
         <wam-separator></wam-separator>
         <wam-item id="three"></wam-item>
       </wam-menu>`
     );
-    list = parent.getInteractiveItems();
-  })
 
+    list = menu.getInteractiveItems();
+    itemOne = menu.querySelector("#one");
+    itemTwo = menu.querySelector('#two');
+    itemThree = menu.querySelector('#three');
+  })
 
   describe("isDefaultFocus", () => {
     it("defaults to false", () => {
@@ -28,7 +31,10 @@ describe("FocusList", () => {
   });
 
   it("does not include separator", async () => {
+    const separator = menu.querySelector('wam-separator');
     expect(list.length).to.equal(3);
+    const array = Array.from(list);
+    expect(array).not.to.include(separator);
   })
 
   it.skip("focusFirst focuses first element", () => {
@@ -40,4 +46,32 @@ describe("FocusList", () => {
 
     expect(document.activeElement === first, "document.activeElement === first").to.be.true;
   })
+
+  describe("next", ()=>{
+    it(`of middle is last`, async () => {
+      list.focusOn(itemTwo);
+      expect(list.next).to.equal(itemThree);
+    });
+  
+    it(`of last is first`, async () => {
+      list.focusOn(itemThree)
+      expect(list.next).to.equal(itemOne);
+    });
+  })
+
+  describe("previous", ()=>{
+    it(`of last is middle`, async () => {
+      list.focusOn(itemThree);
+      expect(list.previous).to.equal(itemTwo);
+    });
+  
+    it(`of first is last)`, async () => {
+      list.focusOn(itemOne)
+      expect(list.previous).to.equal(itemThree);
+    });
+  })
+
+
+
+
 });
