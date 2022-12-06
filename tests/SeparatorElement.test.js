d7a8bbfd4601be7516dd9f9846bff1ab83fa29d6
Mark Dane <mark.dane@gmail.com>
2022-12-06 15:13:38 -0500

    Fix test

--- tests/SeparatorElement.test.js
+++ tests/SeparatorElement.test.js
@@ -1,4 +1,4 @@
-import { expect, fixture, html } from "@open-wc/testing";
+import { expect, fixture } from "@open-wc/testing";
 
 import "../dist/webapp-menu";
 
@@ -7,7 +7,7 @@ describe("SeparatorElement", () => {
 
   it("sets role of separator in shadow root element", async ()=>{
     const element = await fixture(`<wam-separator></wam-separator>`);
-    const separator = element.querySelector(["role=separator]");
+    const separator = element.shadowRoot.querySelector("[role=separator]");
     expect(separator).not.to.be.null;
   })
 });
